10c3a1f0974869bf5c552c6f287ed19c51d4bfd0
unknown <ayrtoon2santos@gmail.com>
2022-09-06 14:10:10 -0300

    Incorporo arrays

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,72 @@
 const name = prompt("Hola guerrero, cual es tu nombre?").toLowerCase();
-const selfPower = Number(prompt("Veo que no estas en la base de datos, dime cual es tu nivel de poder por favor"));
+const selfPower = Number(prompt("Veo que no estas en la base de datos, dime de cuanto es tu nivel de pelea por favor"));
 
 alert(selfPower  + " no es un mal nivel pero podrias mejorar");
 
 const time = Number(prompt("Por cuantos dias te gustaria entrenar?"));
 
-const train = selfPower * time / 0.5;
+const train = selfPower * time / 0.519;
 
 alert("Eso estuvo bien no? ahora tu nivel es de " + train + ", pero pasemos a algo mas dificil!")
 
-alert("Ahora pasaremos a un combate mano a mano, puedes elegir entre estos oponentes.  \n" + "Vegeta, Poder: 50000  \n" + "Goku, Poder 52000  \n" + "Gohan, poder: 35000  \n" + "Krillin, Poder: 12000  \n" + "Piccoro, Poder 18000 \n" + "Y muchos otros peleadores \n" )
+alert("Podras elegir un contrincante entre  los siguientes: " )
 
-let oponentPower = prompt("Que nivel te gustaria que tenga el oponente al que te enfrentaras?")
+for (personaje of personajes) {
+    alert(personaje.name + ": " + personaje.power);      
+}
+
+let personajes = [
+    {name: "Goku",
+    power: 52000,
+    img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png"},
+    
+    {name: "Vegeta",
+    power: 50000,
+    img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png"},
+    
+    {name: "Gohan",
+    power: 34000,
+    img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png"},
+    
+    {name: "Piccoro",
+    power: 22000,
+    img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png"},
+    
+    {name: "Krillin",
+    power: 7000,
+    img: "https://e7.pngegg.com/pngimages/194/55/png-clipart-krillin-goku-beerus-frieza-trunks-disco-ball-hand-vertebrate.png"}
+]
+
+
+let oponentPower = prompt("A quien te gustaria enfrentarte ahora?").toLowerCase()
+
+switch (oponentPower) {
+    case 'krillin':
+        alert("El humano mas fuerte" )
+        break;
+    case 'piccoro':
+        alert("El Namakiano mas poderoso")
+        break;
+    case  'gohan':  
+        alert("No lo hagas enojar")
+        break;
+    case  'vegeta':
+        alert("Es muy orgulloso ten cuidado")
+        break;
+    case  'goku':
+        alert("Le encantan estas batallas")
+        break;
+    default:
+        let reask = prompt("No entendi con quien quieres pelar, o ese guerrero no estaba en la lista, quieres intentar de nuevo?")
+        if (reask === "si") {
+            prompt("A quien te gustaria enfrentarte ?")
+        }else if (reask === "si"){
+            alert("Genial, no es necesario que pelees todavia!!")
+        }else{
+            alert("Demasiados errores, fallo en la matrix")
+        }
+        break;
+}
 
 
 
-if(oponentPower > selfPower){
-    alert("No estas a ese nivel intenta otro")}
-
-while(oponentPower != 0){ 
-    if(oponentPower >= 12000 && oponentPower  <= 18000){
-        alert("Creo que mi amigo Krillin seria un buen oponente")
-    }else if(oponentPower >= 18000 && oponentPower  <= 34999){
-        alert("Creo que Piccoro sera tu oponente")
-    }else if(oponentPower >= 35000 && oponentPower  <= 50000){
-        alert("Creo que Gohan seria una buena opcion")
-    }else if(oponentPower >= 50000 && oponentPower  <= 52000){
-        alert("Pelearas con Vegeta, un gran desafio")
-    }else if(oponentPower >= 52000 && oponentPower  <= 60000){
-        alert("Solo Goku tiene ese nivel, suerte en la batalla")
-    }else if (oponentPower >= 60000){
-        prompt("No hay ningun oponente tan poderoso, prueba con un poder no tan elevado")
-    }else{
-        alert("Supongo que Chaos es la mejor opcion")
-    }
-    break;
-   }
