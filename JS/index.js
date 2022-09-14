17f1824904843233aeb4ea75a72f225b7f0408a1
unknown <ayrtoon2santos@gmail.com>
2022-09-13 23:39:10 -0300

    primer ingreso DOM

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,3 @@
-let nombre = prompt("Hola guerrero, cual es tu nombre?");
-nombre = nombre.toLowerCase();
-
 let personajes = [
     {
         name: "Goku",
@@ -13,7 +10,7 @@ let personajes = [
     {
         name: "Vegeta",
         power: 50000,
-        img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png",
+        img: "https://www.pngmart.com/files/2/Vegeta-PNG-Photos.png",
         dscr: "Es muy orgulloso ten cuidado, no te dejara vencerlo",
         race: "Saiyan"
     },
@@ -21,106 +18,37 @@ let personajes = [
     {
         name: "Gohan",
         power: 34000,
-        img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png",
+        img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/84dc13b7-a2e7-4b45-83ec-311e72e82900/ddmjuhn-d96f723c-2abf-4a03-ae6d-9f3c034a043c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg0ZGMxM2I3LWEyZTctNGI0NS04M2VjLTMxMWU3MmU4MjkwMFwvZGRtanVobi1kOTZmNzIzYy0yYWJmLTRhMDMtYWU2ZC05ZjNjMDM0YTA0M2MucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ld-YmuCCfORBh5jrPUvl5Cf11JMTAF_OLTimXsSvqIQ",
         dscr: "No lo hagas enojar, se dice que su poder puede superar al de su padre y a vegeta",
         race: "Human - Saiyan"
-    },
-
-    {
-        name: "Piccoro",
-        power: 22000,
-        img: "http://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png",
-        dscr: "Es el Namakiano mas poderoso, y uno de los unicos que sobrevivio.",
-        race: "namek"
-    },
-
-    {
-        name: "Krillin",
-        power: 7000,
-        img: "https://e7.pngegg.com/pngimages/194/55/png-clipart-krillin-goku-beerus-frieza-trunks-disco-ball-hand-vertebrate.png",
-        dscr: "Segun el creador, el es el humano mas fuerte",
-        race: "Human"
-    },
-];
-
-
-const selfPower = parseInt(
-    prompt(
-        "Veo que no estas en la base de datos, dime de cuanto es tu nivel de pelea por favor"
-    )
-);
-
-alert(selfPower + " no es un mal nivel pero podrias mejorar");
-
-const time = Number(prompt("Por cuantos dias te gustaria entrenar?"));
-
-function train(days, power) {
-    days = time;
-    power = selfPower;
-    train = power * 1.1 * days;
-    alert(
-        "Eso estuvo bien no? ahora tu nivel es de " +
-        train +
-        ", pero pasemos a algo mas dificil!"
-    );
-}
-
-train();
-
-alert("Podras elegir un contrincante entre  los siguientes: ");
-
-for (const personaje of personajes) {
-    alert(personaje.name + ": " + personaje.power);
-}
-
-function reask() {
-    let tryAgain = prompt("No entendi con quien quieres pelar, o ese guerrero no estaba en la lista, quieres intentar de nuevo? contesta si o no");
-    if (tryAgain === "si") {
-        oponent();
-    } else if (tryAgain === "no") {
-        alert("Genial, no es necesario que pelees todavia!!");
-    } else {
-        alert("Demasiados errores, fallo en la matrix");
     }
+]
+
+let userStat = {
+    name: prompt("¿Como te LLamas?"),
+    power: prompt("¿Cuanto poder de pelea tienes?"),
+    img: "https://w7.pngwing.com/pngs/529/687/png-transparent-person-illustration-human-body-computer-icons-anatomy-homo-sapiens-human-icon-miscellaneous-hand-human.png",
+    dscr: "Un simple humano",
+    race: "Humano"
 }
 
+personajes.push(userStat)
 
-function oponent() {
-    let oponentChosen = prompt("A quien te gustaria enfrentarte ahora?").toLowerCase();
-    let personaje = personajes.find(pj => pj.name === oponentChosen);
-    switch (oponentChosen) {
-        case "krillin":
-            oponentChosen = capitalizeFirstLetter(oponentChosen);
-            personaje = personajes.find(pj => pj.name === oponentChosen);
-            alert(`${oponentChosen}, tiene ${personaje.power} de poder, dato curioso: ${personaje.dscr}`);
-            break;
-        case "piccoro":
-            oponentChosen = capitalizeFirstLetter(oponentChosen);
-            personaje = personajes.find(pj => pj.name === oponentChosen);
-            alert(`${oponentChosen}, tiene ${personaje.power} de poder, dato curioso: ${personaje.dscr}`);
-            break;
-        case "gohan":
-            oponentChosen = capitalizeFirstLetter(oponentChosen);
-            personaje = personajes.find(pj => pj.name === oponentChosen);
-            alert(`${oponentChosen}, tiene ${personaje.power} de poder, dato curioso: ${personaje.dscr}`);
-            break;
-        case "vegeta":
-            oponentChosen = capitalizeFirstLetter(oponentChosen);
-            personaje = personajes.find(pj => pj.name === oponentChosen);
-            alert(`${oponentChosen}, tiene ${personaje.power} de poder, dato curioso: ${personaje.dscr}`);
-            break;
-        case "goku":
-            oponentChosen = capitalizeFirstLetter(oponentChosen);
-            personaje = personajes.find(pj => pj.name === oponentChosen);
-            alert(`${oponentChosen}, tiene ${personaje.power} de poder, dato curioso: ${personaje.dscr}`);
-            break;
-        default:
-            reask();
-    }
-}
-oponent();
+function personajeCard(personajes) {
 
-function capitalizeFirstLetter(str) {
-    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
-    return capitalized;
+    const pjCard = document.getElementById("contCards");
+
+    pjCard.innerHTML = "";
+
+    personajes.forEach(personaje => {
+        const divPj = document.createElement("div");
+        divPj.classList.add("pj");
+        divPj.innerHTML = `
+        <h3 class="tituloPJ">${personaje.name}</h3>
+        <img src="${personaje.img}" alt="${personaje.dscr}">
+        <p class="powerPj"> ${personaje.power}</p>
+        `;
+        pjCard.append(divPj);
+    })
 }
+personajeCard(personajes);
